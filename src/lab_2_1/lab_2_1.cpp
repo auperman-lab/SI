d1b9e02137306880b12206467bd3fea831d47ea4
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-03-06 19:57:55 +0200

    task2 for lab 2.1 done

--- src/lab_2_1/lab_2_1.cpp
+++ src/lab_2_1/lab_2_1.cpp
@@ -5,10 +5,15 @@
 #include "srv/srv_serial/srv_serial.h"
 
 Led led_green(10);
+Led led_red(9);
+
+
+Button button1(5);
+Button button2(4);
+Button button3(3);
 
 
 void lab_2_1_setup(){
-    button_init();
     srv_serial_setup();
 
     printf("Setup for lab 2.1 done\n");  
@@ -21,18 +26,35 @@ void lab_2_1_loop(){
 }
 
 void lab_2_1_task_1(){
-    printf("Lab 2.1 task loop is working");
-    if(button_is_pressed()){
+    printf("Lab 2.1 task1 loop is working");
+    if(button1.is_pressed()){
 
         if(led_green.is_on()){
             led_green.off();
         }else{
             led_green.on();
         }
+    };
 
+    if(led_green.is_on()){
+        led_red.off();
     };
 }
 
 void lab_2_1_task_2(){
+    if(!led_green.is_on()){
+        if(led_red.is_on()){
+            led_red.on();
+        }else{
+            led_red.off();
+        }
+
+    }else{
+        led_red.off();
+    }
+
+}
+
+void lab_2_1_task_3(){
 
 }
