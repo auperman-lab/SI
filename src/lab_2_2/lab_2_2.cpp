25f56fd6a79c66e0266b4814e9749f436153289a
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-03-18 12:05:06 +0200

    lab 2.2 done

--- src/lab_2_2/lab_2_2.cpp
+++ src/lab_2_2/lab_2_2.cpp
@@ -15,6 +15,9 @@ Button button(5);
 int task_1_led_counter  = 0;
 SemaphoreHandle_t xSemaphore = NULL;
 
+int task_2_n_counter = 0;
+QueueHandle_t xQueue = NULL;
+
 void task_1(void *pvParameters){
 
     TickType_t xLastTimeAwake = xTaskGetTickCount();
@@ -22,7 +25,7 @@ void task_1(void *pvParameters){
 
     while (1){
         printf("task1\n");
-        if (button.is_pressed() && task_1_led_counter <0){
+        if (button.is_pressed() && task_1_led_counter <=0){
             task_1_led_counter = 1000/ TASK_1_REC;
             led_1.on();
             printf("button press detected\n");
@@ -51,18 +54,65 @@ void task_1(void *pvParameters){
 void task_2(void *pvParameters){
 
     while(1){
+
         if(xSemaphoreTake(xSemaphore, portMAX_DELAY)==pdTRUE){
-            
+
+            task_2_n_counter++;
+            for (int i=0; i<task_2_n_counter;i++){
+                xQueueSend(xQueue, &i, portMAX_DELAY);
+                vTaskDelay(50/portTICK_PERIOD_MS);
+            }
+
+            for (int i=0; i< task_2_n_counter;i++){
+                led_2.on();
+                vTaskDelay(300/portTICK_PERIOD_MS);
+                led_2.off();
+                vTaskDelay(500/portTICK_PERIOD_MS);
+                printf("Led blitz\n");
+            }
+
+        }
+
+        vTaskDelay(500/portTICK_PERIOD_MS);
+
+
+
+    }
+
+}
+
+void task_3(void *pvParameters){
+    while(1){
+        vTaskDelay(200/portTICK_PERIOD_MS);
+        int data;
+        bool bufferNotEmpty = false;
+
+        while (xQueueReceive(xQueue, &data, 200/portTICK_PERIOD_MS)==pdTRUE){
+            if (data == 0){
+                printf("Task 3: Buffer Content: ");
+            }
+            printf(" %d, ", data);
+            bufferNotEmpty = true;
+        }
+        if (!bufferNotEmpty){
+            printf("Task 3: Buffer Empty\n");
+
         }
     }
 
 }
 
 
+
 void lab_2_2_setup(){
     srv_serial_setup();
+    xQueue = xQueueCreate(10, sizeof(int));
+    xSemaphore = xSemaphoreCreateBinary();
+
     xTaskCreate(task_1, "task_1", 1000, NULL, 1, NULL);
     xTaskCreate(task_2, "task_2", 1000, NULL, 1, NULL);
+    xTaskCreate(task_3, "task_3", 1000, NULL, 1, NULL);
+
 
     vTaskStartScheduler();
 
--- src/lab_2_2/lab_2_2.h
+++ src/lab_2_2/lab_2_2.h
@@ -4,8 +4,8 @@
 #define TASK_1_REC 10/portTICK_PERIOD_MS
 #define TASK_1_OFFSET 100/portTICK_PERIOD_MS
 
-#define TASK_1_REC 50
-#define TASK_1_OFFSET 50
+#define TASK_2_REC 50
+#define TASK_2_OFFSET 50
 
 void lab_2_2_setup();
 
